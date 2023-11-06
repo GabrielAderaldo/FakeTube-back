1fc8ee0a1dc2d53831634bb7d9666229ad17bafa
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2023-11-06 18:59:33 -0300

    [01] - Criando as rotas de registro e de Login.

---
+++ src/Controller/userController.ts
@@ -0,0 +1,7 @@
+class UserController{
+
+    getUserInformatio(){}
+
+}
+
+export default UserController
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-import DbClient from './db/dbClient'
-import TypeOfConnections from './db/dbEnum'
-import Cell from './cells/cells'
+import DbClient from './infra/db/dbClient'
+import TypeOfConnections from './infra/db/dbEnum'
+import Cell from './infra/cells/cells'
 import AuthController from './Controller/authController'
+import NodeMailerService from './infra/mail/NodemailerService'
+import TrasporterSingleton from './infra/mail/trasporterSingleton'
 require('dotenv').config()
 
 const port = process.env.PORT || 3000
@@ -12,16 +14,19 @@ const app = express()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 const authController = new AuthController()
-//const clientCell = new Cell(cell ?? '')
-//clientCell.choseCellToUse()
+
 const client = new DbClient({typeOfConnection:TypeOfConnections.URL}
     ,{database:'',host:'',password:'',port:0,user:''}
     ,{url:process.env.WITH_URL_DB_IS_CONNECTED!})
 
+
+
+
 app.post('/register',authController.register.bind(authController))
+app.post('/login',authController.login.bind(authController))
+app.get('/aaa',authController.forgotPass.bind(authController))
 
 app.listen(port,function(){
     console.log('Iniciando o servidor na porta '+port)
     client.connect()
-
 })
